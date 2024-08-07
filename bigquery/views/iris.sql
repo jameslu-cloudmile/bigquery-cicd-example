b72cc0a2ba52e82c84d558348dbb673f33f6fe35
JamesLu <jameslu@JamesLudeMacBook-Pro.local>
2024-08-07 11:53:07 +0800

    modify iris query

--- bigquery/views/iris.sql
+++ bigquery/views/iris.sql
@@ -1,8 +1,9 @@
 SELECT 
    variety, 
+   sepalLength, 
    AVG(petalLength) AS avg_petal_length,
    AVG(petalWidth) AS avg_petal_width
 FROM 
    `tw-rd-tam-jameslu.test.iris`
 GROUP BY 
-   variety
+   variety, sepalLength
