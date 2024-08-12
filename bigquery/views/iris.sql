650970f012b292d18567936c843c80ea0687f27c
JamesLu <jameslu@JamesLudeMacBook-Pro.local>
2024-08-12 11:04:14 +0800

    add file change detection 7

--- bigquery/views/iris.sql
+++ bigquery/views/iris.sql
@@ -2,7 +2,7 @@ SELECT
    variety, 
    AVG(petalLength) AS avg_petal_length,
    AVG(petalWidth) AS avg_petal_width,
-   -- MAX(petalLength) AS max_petal_length,
+   MAX(petalLength) AS max_petal_length,
    MIN(petalLength) AS min_petal_length
    -- MAX(petalWidth) AS max_petal_width,
    -- MIN(petalWidth) AS min_petal_width
