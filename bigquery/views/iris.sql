58a1a1a0d42e3e7c0318a06b4e05dce53ccc1fa3
JamesLu <jameslu@JamesLudeMacBook-Pro.local>
2024-08-12 14:41:07 +0800

    modify iris query 10

--- bigquery/views/iris.sql
+++ bigquery/views/iris.sql
@@ -3,7 +3,7 @@ SELECT
    AVG(petalLength) AS avg_petal_length,
    AVG(petalWidth) AS avg_petal_width,
    MAX(petalLength) AS max_petal_length,
-   MIN(petalLength) AS min_petal_length,
+   -- MIN(petalLength) AS min_petal_length,
    MAX(petalWidth) AS max_petal_width,
    MIN(petalWidth) AS min_petal_width
 FROM 
