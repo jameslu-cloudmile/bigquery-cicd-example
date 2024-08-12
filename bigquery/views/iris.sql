96dd355bdbb0a7ae19864a0f8fdcc22ecc864c4c
JamesLu <jameslu@JamesLudeMacBook-Pro.local>
2024-08-12 09:56:50 +0800

    modify iris query 4

--- bigquery/views/iris.sql
+++ bigquery/views/iris.sql
@@ -1,6 +1,11 @@
 SELECT 
    variety, 
-   AVG(petalLength) AS avg_petal_length
+   AVG(petalLength) AS avg_petal_length,
+   AVG(petalWidth) AS avg_petal_width
+   MAX(petalLength) AS max_petal_length,
+   MIN(petalLength) AS min_petal_length,
+   MAX(petalWidth) AS max_petal_width,
+   MIN(petalWidth) AS min_petal_width
 FROM 
    `tw-rd-tam-jameslu.test.iris`
 GROUP BY 
